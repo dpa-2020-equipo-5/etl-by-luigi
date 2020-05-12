0a984d1c9e270e53b8084c004fb7f37be46ab76a
Gerardo Mathus <gerardo@nextia.mx>
2020-05-11 20:20:26 -0500

    add luigi log level

--- bin/run_aws.sh
+++ bin/run_aws.sh
@@ -1,1 +1,1 @@
-PYTHONPATH='.' AWS_PROFILE=mathus_itam luigi --module nyc_ccci_etl.luigi_tasks.models_creator ModelsCreator --year=$1 --month=$2 --day=$3
+PYTHONPATH='.' AWS_PROFILE=mathus_itam luigi --log-level=INFO --module nyc_ccci_etl.luigi_tasks.models_creator ModelsCreator --year=$1 --month=$2 --day=$3
