31fe5b9e9a6f2794883f07c65058adeb8df36d26
Gerardo Mathus <gerardo@nextia.mx>
2020-06-02 21:00:26 -0500

    add new

--- nyc_ccci_etl/tests/test_predictions.py
+++ nyc_ccci_etl/tests/test_predictions.py
@@ -14,8 +14,7 @@ class TestPredictions(TestCase):
         predictions = predictor.output_table
         ran_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         try:
-            self.assertEquals(len(predictions.columns), 5, note="Las predicciones deben tener las columnas center_id, probability, date, priority, matrix_uuid.")
-            
+            self.assertEquals(len(predictions.columns), 6, note="Las predicciones deben tener las columnas center_id, probability, date, priority, matrix_uuid.")
             return {
                 "test":"test_predictions_have_correct_columns",
                 "status":"passed",
